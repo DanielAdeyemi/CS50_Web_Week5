f07da28982b3ba99748bd82fedb573ac938afd7c
DanielAdeyemi <adeyemidany@gmail.com>
2022-08-14 16:50:47 -0700

    create login view

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.index, name='login'),
+    path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+
+def login(request):
+    return render(request, 'users/login.html')
