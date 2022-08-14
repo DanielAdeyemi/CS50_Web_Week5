9f74c0eebd4101f935dbfd8768ca3e6fe1ec8e5b
DanielAdeyemi <adeyemidany@gmail.com>
2022-08-14 14:27:32 -0700

    update views to display all the flights

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Flight, Airport
 
-# Create your views here.
+
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
