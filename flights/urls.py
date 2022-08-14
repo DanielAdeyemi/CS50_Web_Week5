fa0b753cfad83b3af08f2a81417254157d73b947
DanielAdeyemi <adeyemidany@gmail.com>
2022-08-14 15:29:29 -0700

    add details for specific flight

--- flights/urls.py
+++ flights/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('<int:flight_id>', views.flight, name="flight")
 ]
